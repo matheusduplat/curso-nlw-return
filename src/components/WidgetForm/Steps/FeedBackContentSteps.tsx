d95a1a1b375fdd24ccbe3bf43289ff2e6a17b581
Matheus Duplat <matheusduplat@hotmail.com>
2022-05-11 13:38:46 -0300

    finalizando aplicação

--- src/components/WidgetForm/Steps/FeedBackContentSteps.tsx
+++ src/components/WidgetForm/Steps/FeedBackContentSteps.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloserButtom } from "../../CloserButtom";
 import { ScreensShotButton } from "../ScreensShotButton";
@@ -7,15 +7,24 @@ import { ScreensShotButton } from "../ScreensShotButton";
 interface FeedBackContentStepsProps {
   feedbackType: FeedbackType;
   onFeedbackRestartResquested: () => void;
+  onFeedbackSent: () => void;
 }
 
 export function FeedBackContentSteps({
   feedbackType,
   onFeedbackRestartResquested,
+  onFeedbackSent,
 }: FeedBackContentStepsProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [coment, setComent] = useState("");
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault();
+    console.log({ coment, screenshot });
+    onFeedbackSent();
+  }
 
   return (
     <>
@@ -38,10 +47,11 @@ export function FeedBackContentSteps({
         <CloserButtom />
       </header>
 
-      <form className="my-4 w-full">
+      <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
         <textarea
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que ta acontecendo..."
+          onChange={(event) => setComent(event.target.value)}
         />
         <footer className="flex gap-2 mt-2">
           <ScreensShotButton
@@ -49,8 +59,9 @@ export function FeedBackContentSteps({
             screenshot={screenshot}
           />
           <button
+            disabled={coment.length === 0}
             type="submit"
-            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
             Enviar FeedBack
           </button>
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,6 +5,7 @@ import thoughtImageUrl from "../../assets/img/thought.svg";
 import { useState } from "react";
 import { FeedBackTypeSteps } from "./Steps/FeedBackTypeSteps";
 import { FeedBackContentSteps } from "./Steps/FeedBackContentSteps";
+import { FeedBackSuccessSteps } from "./Steps/FeedBackSuccessStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -34,20 +35,31 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestartFeedback() {
+    setFeedbackSent(false);
     setFeedbackType(null);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      {!feedbackType ? (
-        <FeedBackTypeSteps onFeedBackTypeChanger={setFeedbackType} />
-      ) : (
-        <FeedBackContentSteps
-          feedbackType={feedbackType}
+      {feedbackSent ? (
+        <FeedBackSuccessSteps
           onFeedbackRestartResquested={handleRestartFeedback}
         />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedBackTypeSteps onFeedBackTypeChanger={setFeedbackType} />
+          ) : (
+            <FeedBackContentSteps
+              feedbackType={feedbackType}
+              onFeedbackRestartResquested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
       <footer className="text-xs text-neutral-400">
         Feito com ♥ por{" "}
